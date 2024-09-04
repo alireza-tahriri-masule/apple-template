a888ac9656b94d952b95ab3191d7af24aa71cac4
Alireza Tahriri Masule <alirezatahriri4@gmail.com>
2024-09-05 01:02:07 +0330

    feat(app): Add hero section Add video and buy button
    Add 2 iPhone video for sm, lg screen and develop a buy button with animation and its price
    
    Closes: Add hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,49 @@
 <script>
-export default {};
+import { gsap } from "gsap";
+import { ref, computed, onMounted, watchEffect } from 'vue';
+import { heroVideo, smallHeroVideo } from "../utils";
+
+export default {
+  setup() {
+    const videoSrc = ref(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+
+    // برای به‌روزرسانی ویدیو بر اساس تغییر اندازه صفحه
+    const updateVideoSrc = () => {
+      videoSrc.value = window.innerWidth < 760 ? smallHeroVideo : heroVideo;
+    };
+
+    watchEffect(() => {
+      window.addEventListener('resize', updateVideoSrc);
+    });
+
+    onMounted(() => {
+      gsap.to("#hero", { opacity: 1, delay: 2 });
+      gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+    });
+
+    return {
+      videoSrc: computed(() => videoSrc.value)
+    };
+  },
+};
 </script>
 
 <template>
-  <div>Hero</div>
+  <section class="w-full nav-height bg-black relative">
+    <div class="h-5/6 w-full flex-center flex-col">
+      <p id="hero" class="hero-title">iPhone 15 Pro</p>
+      <div class="md:w-10/12 w-9/12">
+        <video class="pointer-events-none" autoplay muted playsinline="true" :key="videoSrc">
+          <source :src="videoSrc" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+
+    <div id="cta" class="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#hightlights" class="btn">Buy</a>
+        <p class="font-normal text-xl">From $199/month or $999</p>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
